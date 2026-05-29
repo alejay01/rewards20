ab5e7e9d244bb998030b8d29ad673012da6bd8cc
alejay01 <alejay01@github.com>
2026-05-28 21:31:27 -0500

    Promoted_all_Vite_and_PostCSS_styling_devDependencies_to_dependencies_and_fixed_dotenv_override

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,9 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config({ override: true });
 const app_1 = require("./app");
-dotenv_1.default.config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const server = app_1.app.listen(PORT, () => {
     console.log(`===============================================`);
     console.log(`   Boudin Boss Rewards MVP Server is running!`);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
+dotenv.config({ override: true });
+
 import { app } from "./app";
 
-dotenv.config();
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const server = app.listen(PORT, () => {
   console.log(`===============================================`);
